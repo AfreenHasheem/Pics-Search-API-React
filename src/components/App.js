6da73f4749d116d5af686725e044df40a3d5bcb9
Afreen Siraj <afreen.siraj@capco.com>
2019-10-03 14:39:40 +0100

    Adding axios and .then()

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
 
-    return (
-        <div className="ui container" style={{ marginTop: '10px'}}>
-            <SearchBar />
-        </div>
-    )
-};
+class App extends React.Component {
+    onSearchSubmit(term){
+       axios.get('https://api.unsplash.com/search/photos',{
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 8000f9644ddd29ad65ad5c28a6fc4685c69b65d98fd9f1dee6c6ee659e1c58de'
+            }
+       }).then(response => {
+           console.log(response.data.results);
+       })
+    }
+
+    render(){
+        return (
+            <div className="ui container" style={{ marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    } 
+}
 
 export default App;
 
